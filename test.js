859542c0672ff5218344b1395ad37c8f6e4c0e9f
Hesham <heshamusama@gmail.com>
2016-12-21 22:02:08 +0200

    testing clusters

--- test.js
+++ test.js
@@ -1,8 +1,43 @@
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+var _ = require('lodash');
+var a = [];
+var n = 0;
+if (cluster.isMaster) {
+	var s = 'asdwqsddwggw';
+	var u = _.uniq(s).length;
+	console.log('u ', u);
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+  	n = i;
+  	a.push(n);
+    var worker = cluster.fork();
+    worker.send({
+    	i: i
+    });
+  }
 
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    if (worker.id===3){
+    	cluster.fork();
+    }
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case it is an HTTP server
+  console.log('cluster.worker', cluster.worker.id);
+  // if(cluster.worker.id == 3) {
+  // 	process.exit(0);
+  // }
+  process.on('message', (msg) => {
+    console.log(msg.i + ' / ' + numCPUs);
+  });
+}
   
 
 
-var lineReader = require('readline').createInterface({
+/*var lineReader = require('readline').createInterface({
 	input: require('fs').createReadStream('./wordlist')
 });
 var regex = /[a-z]/;
@@ -28,4 +63,4 @@ lineReader.on('line', function(word) {
 lineReader.on('close', function() {
 	console.log('chars: ', chars);
 	console.log('lookouts: ', lookouts);
-});
+});*/
